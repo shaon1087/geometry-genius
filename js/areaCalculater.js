86ac38575c9faee70c3048821e1b57e6d3273922
shaon1087 <shaonsaha1087@gmail.com>
2024-02-29 14:12:58 +0600

    parrallelogam added

--- js/areaCalculater.js
+++ js/areaCalculater.js
@@ -1,34 +1,48 @@
-function triangleCalculator(){
+function triangleCalculator() {
+  const triangleFieldBase = document.getElementById("triangle-base");
+  const triangleFieldBaseText = triangleFieldBase.value;
+  const triangleFieldBaseValue = parseFloat(triangleFieldBaseText);
 
-    const triangleFieldBase = document.getElementById('triangle-base');
-    const triangleFieldBaseText = triangleFieldBase.value;
-    const triangleFieldBaseValue = parseFloat(triangleFieldBaseText);
+  const triangleFieldHeight = document.getElementById("triangle-height");
+  const triangleFieldHeightText = triangleFieldHeight.value;
+  const triangleFieldHeightValue = parseFloat(triangleFieldHeightText);
 
+  const triangleArea = triangleFieldBaseValue * 0.5 * triangleFieldHeightValue;
 
-    const triangleFieldHeight = document.getElementById('triangle-height');
-    const triangleFieldHeightText = triangleFieldHeight.value;
-    const triangleFieldHeightValue = parseFloat(triangleFieldHeightText);
-
-    const triangleArea =
-      triangleFieldBaseValue * 0.5 * triangleFieldHeightValue;
-
-      const areaSpan = document.getElementById('triangle-area');
-      areaSpan.innerText = triangleArea;
-
+  const areaSpan = document.getElementById("triangle-area");
+  areaSpan.innerText = triangleArea;
 }
 
-function rectangleCalculator(){
-    const rectangleFieldWidth = document.getElementById("rectangle-width");
-    const rectangleFieldWidthText = rectangleFieldWidth.value;
-    const rectangleFieldWidthValue = parseFloat(rectangleFieldWidthText);
+function rectangleCalculator() {
+  const rectangleFieldWidth = document.getElementById("rectangle-width");
+  const rectangleFieldWidthText = rectangleFieldWidth.value;
+  const rectangleFieldWidthValue = parseFloat(rectangleFieldWidthText);
 
-    const  rectangleFieldLength = document.getElementById('rectangle-length');
-    const rectangleFieldText =  rectangleFieldLength.value;
-    const rectangleFieldLengthValue = parseFloat(rectangleFieldText);
+  const rectangleFieldLength = document.getElementById("rectangle-length");
+  const rectangleFieldText = rectangleFieldLength.value;
+  const rectangleFieldLengthValue = parseFloat(rectangleFieldText);
 
-    const triangleArea = rectangleFieldWidthValue * rectangleFieldLengthValue;
+  const triangleArea = rectangleFieldWidthValue * rectangleFieldLengthValue;
 
-    const areaSpan = document.getElementById("rectangle-area");
-    areaSpan.innerText = triangleArea;
+  const areaSpan = document.getElementById("rectangle-area");
+  areaSpan.innerText = triangleArea;
+}
 
+function parallelogramCalculator() {
+  const parallelogramBase = getInputValueById("parallelogram-b");
+  const parallelogramLength = getInputValueById("parallelogram-h");
+  const parallelogramArea = parallelogramBase * parallelogramLength;
+  setTextFieldText("parallelogram-area", parallelogramArea);
+}
+
+function getInputValueById(inputId) {
+  const inputValueField = document.getElementById(inputId);
+  const inputValueText = inputValueField.value;
+  const inputValue = parseFloat(inputValueText);
+  return inputValue;
+}
+
+function setTextFieldText(areaId, area) {
+  const TextArea = document.getElementById(areaId);
+  TextArea.innerText = area;
 }
